44f6a46f3fb6e249559d647c6fc35f89f55dc09a
IXSTERNE <ixsterne@gmail.com>
2024-02-22 16:00:08 +0800

    update

--- database_model.py
+++ database_model.py
@@ -1,5 +1,6 @@
 class Books:
 
+    # Didn't use *args because it's usually a positional argument
     def __init__(self, **kwargs):
         self.book_name = kwargs.get('book_name')
         self.book_author = kwargs.get('book_author')
@@ -10,3 +11,21 @@ class Books:
         self.book_dimension = kwargs.get('book_dimension')
         self.book_ISBN = kwargs.get('book_ISBN')
         self.book_page_count = kwargs.get('book_page_count')
+        self.book_synopsis = kwargs.get('book_synopsis')
+        self.book_cover_type = kwargs.get('book_cover_type')
+        self.book_price = kwargs.get('book_price')
+        self.book_model = kwargs.get('book_model')
+        self.book_stock = kwargs.get('book_stock')
+        self.book_weight = kwargs.get('book_weight')
+
+class Sells:
+
+    def __init__(self):
+        pass
+        
+
+class Report:
+
+    def __init__(self):
+        pass
+        
