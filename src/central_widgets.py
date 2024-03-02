373acff4d4785f7a0e1a73f215c8dca679d830ba
IXSTERNE <ixsterne@gmail.com>
2024-03-02 20:51:03 +0800

    shader/ongoing

--- src/central_widgets.py
+++ src/central_widgets.py
@@ -165,6 +165,7 @@ class CentralWidget(QtWidgets.QWidget):
         importer.SetRenderWindow(self.vtkWidget.GetRenderWindow())
         importer.Update()
 
+
         # Annotations
 
         self.textActor = vtk.vtkTextActor()
@@ -174,133 +175,74 @@ class CentralWidget(QtWidgets.QWidget):
 
         self.renderer.AddActor(self.textActor)
 
-        # Shader
+        # Shader, if it works ----------------------------------------------
 
-        # If this is still a case of shader
+        self.placeHolderActor = vtk.vtkActor()
+        self.shaderProperty = self.placeHolderActor.GetShaderProperty()
 
-        # -> Use built-in shader methods to replace shader strings
-        # -> Completely override the shader methods
+        self.shaderProperty.SetFragmentShaderCode(
 
-        # Take a look at this url -> https://examples.vtk.org/site/Cxx/Shaders/BozoShader/
-
-
-        vertexShader = """
-
-            void main(){
-
-                // This just has to return the uvCoordinate/u_resolution
-                return fragCoord;
-            }
-            
-        """
-
-        fragmentShader = """
-
-            #version 150
-
-            #ifdef GL_ES
-            precision mediump float;
-            #endif
-            
-            precision mediump float;
-            uniform float u_time;
-            uniform vec2 u_resolution;
-            uniform sampler2D u_tex0;
-            out vec4 FragColor;
-            
-            vec2 curveEffect(vec2 uv){
-            
-            	uv = (uv - 0.5) * 2.0;
-            	uv *= 1.0;	// Change this if you want to config distance
-            	uv.x *= 1.0 + pow((abs(uv.y) / 5.0), 2.0); 
-            	uv.y *= 1.0 + pow((abs(uv.x) / 5.0), 2.0);
-            	uv  = (uv / 2.0) + 0.5;
-            	uv =  uv * 0.92 + 0.04;
-            	return uv;
-            }
-            
-            void main(){
-            
-                vec2 fragCoord = gl_FragCoord.xy;
-                vec4 finalColor;
-            
-                vec2 uv = curveEffect(fragCoord.xy / u_resolution.xy);
-                vec3 color;
-            
-                float sinComponent1 = sin(0.3 * u_time + uv.y * 21.0);
-                float sinComponent2 = sin(0.7 * u_time + uv.y * 29.0);
-                float sinComponent3 = sin(0.3 + 0.33 * u_time + uv.y * 31.0);
-            
-                float x = sinComponent1 * sinComponent2 * sinComponent3 * 0.002;
-            
-                color.r = texture(u_tex0, vec2(x + uv.x + 0.001, uv.y + 0.001)).x + 0.05;
-                color.g = texture(u_tex0, vec2(x + uv.x + 0.000, uv.y - 0.002)).y + 0.05;
-                color.b = texture(u_tex0, vec2(x + uv.x - 0.002, uv.y + 0.000)).z + 0.05;
-            
-                color.r += 0.08 * texture(u_tex0, 0.75 * vec2(x + 0.025, -0.027) + vec2(uv.x + 0.001, uv.y + 0.001)).x;
-                color.g += 0.05 * texture(u_tex0, 0.75 * vec2(x + -0.022, -0.02) + vec2(uv.x + 0.000, uv.y - 0.002)).y;
-                color.b += 0.08 * texture(u_tex0, 0.75 * vec2(x + -0.02, -0.018) + vec2(uv.x - 0.002, uv.y + 0.000)).z;
-            
-                color = clamp(color * 0.6 + 0.4 * color * color * 1.0, 0.0, 1.0);
-            
-                float vignette = (1.0 * 16.0 * uv.x * uv.y * (1.0 - uv.x) * (1.0 - uv.y));
-            	color *= vec3(pow(vignette, 0.3));
-            
-            
-                color *= vec3(0.95, 1.05, 0.95);
-            	color = mix(color, color * color, 0.3) * 3.8;
-            
-            	float scanlineEffect = clamp(0.35 + 0.20 * sin(3.5 * u_time + uv.y * u_resolution.y * 1.5), 0.0, 1.0);
-            	
-            	float scanlinePower = pow(scanlineEffect, 1.7);
-            	color = color * vec3(0.4 + 0.7 * scanlinePower) ;
-            
-                color *= 1.0 + 0.01 * sin(200.0 * u_time);
-            
-            	if(uv.x < 0.0 || uv.x > 1.0){
-                    color *= 0.0;
-                }
-            		
-            	if(uv.y < 0.0 || uv.y > 1.0){
-                    color *= 0.0;
-                }
-            		
-            	color *= 1.0 - 0.65 * vec3(clamp((mod(fragCoord.x, 2.0) -1.0) * 2.0, 0.0, 1.0));
-            
-                finalColor = vec4(color, 2.0);
-                FragColor = finalColor;
-            }
-        
-        """
-        
-
-        
-        self.shaderProperty = self.mapper.getShaderProperty()
-        
-        # 
-        self.shaderProperty.replaceShaderProperty(
-            vtk.vtkShaderType().Fragment,
-            True,
-            //VTK::Normal::Dec,
-            vertexShader,
-            False
+        "//VTK::System::Dec\n"
+        "#version 330\n"
+        "#ifdef GL_ES\n"
+        "precision mediump float;\n"
+        "#endif\n"
+        "precision mediump float;\n"
+        "uniform float u_time;\n"
+        "uniform vec2 u_resolution;\n"
+        "uniform sampler2D u_tex0;\n"
+        "out vec4 FragColor;\n"
+        "//VTK::Normal::Dec\n"
+        "vec2 curveEffect(vec2 uv){\n" 
+            "uv = (uv - 0.5) s* 2.0;\n"
+            "uv *= 1.0;\t// Change this if you want to config distance\n"
+            "uv.x *= 1.0 + pow((abs(uv.y) / 5.0), 2.0);\n"
+            "uv.y *= 1.0 + pow((abs(uv.x) / 5.0), 2.0);\n"
+            "uv  = (uv / 2.0) + 0.5;\n"
+            "uv =  uv * 0.92 + 0.04;\n"
+            "return uv;\n"
+        "}\n"
+        "void main(){\n"
+            "vec2 fragCoord = gl_FragCoord.xy;\n" 
+            "vec4 finalColor;\n" 
+            "vec2 uv = curveEffect(fragCoord.xy / u_resolution.xy);\n" 
+            "vec3 color;\n"
+            "float sinComponent1 = sin(0.3 * u_time + uv.y * 21.0);\n" 
+            "float sinComponent2 = sin(0.7 * u_time + uv.y * 29.0);\n" 
+            "float sinComponent3 = sin(0.3 + 0.33 * u_time + uv.y * 31.0);\n"
+            "float x = sinComponent1 * sinComponent2 * sinComponent3 * 0.002;\n" 
+            "color.r = texture(u_tex0, vec2(x + uv.x + 0.001, uv.y + 0.001)).x + 0.05;\n" 
+            "color.g = texture(u_tex0, vec2(x + uv.x + 0.000, uv.y - 0.002)).y + 0.05;\n" 
+            "color.b = texture(u_tex0, vec2(x + uv.x - 0.002, uv.y + 0.000)).z + 0.05;\n" 
+            "color.r += 0.08 * texture(u_tex0, 0.75 * vec2(x + 0.025, -0.027) + vec2(uv.x + 0.001, uv.y + 0.001)).x;\n" \
+            "color.g += 0.05 * texture(u_tex0, 0.75 * vec2(x + -0.022, -0.02) + vec2(uv.x + 0.000, uv.y - 0.002)).y;\n" \
+            "color.b += 0.08 * texture(u_tex0, 0.75 * vec2(x + -0.02, -0.018) + vec2(uv.x - 0.002, uv.y + 0.000)).z;\n" \
+            "color = clamp(color * 0.6 + 0.4 * color * color * 1.0, 0.0, 1.0);\n"
+            "float vignette = (1.0 * 16.0 * uv.x * uv.y * (1.0 - uv.x) * (1.0 - uv.y));\n"
+            "color *= vec3(pow(vignette, 0.3));\n" 
+            "color *= vec3(0.95, 1.05, 0.95);\n"
+            "color = mix(color, color * color, 0.3) * 3.8;\n"
+            "float scanlineEffect = clamp(0.35 + 0.20 * sin(3.5 * u_time + uv.y * u_resolution.y * 1.5), 0.0, 1.0);\n"
+            "float scanlinePower = pow(scanlineEffect, 1.7);\n"
+            "color = color * vec3(0.4 + 0.7 * scanlinePower) ;\n"
+            "color *= 1.0 + 0.01 * sin(200.0 * u_time);\n"
+            "if(uv.x < 0.0 || uv.x > 1.0){\n"
+                "color *= 0.0;\n"
+            "}\n"
+            "if(uv.y < 0.0 || uv.y > 1.0){\n"
+                "color *= 0.0;\n" 
+            "}\n" 
+            "color *= 1.0 - 0.65 * vec3(clamp((mod(fragCoord.x, 2.0) -1.0) * 2.0, 0.0, 1.0));\n"
+            "finalColor = vec4(color, 2.0);\n"
+            "FragColor = finalColor;\n"
+        "}\n"
         )
-
-        self.renderer.replaceShaderProperty(
-            vtk.vtkShaderType().Vertex,
-            True,
-            //VTK::Normal::Impl,
-            fragmentShader,
-            False
-        )
-        
-        
         
         # ------------------------------------------------------------------------
 
         self.renderWindowInteractor = self.vtkWidget.GetRenderWindow().GetInteractor()
         style = vtk.vtkInteractorStyleTrackballCamera()
-        self.renderWindowInteractor.SetInteractorStyle(style)
+        # self.renderWindowInteractor.SetInteractorStyle(style)
 
         self.renderer.SetBackground(0.5, 0.5, 0.5)
         self.renderer.ResetCamera()
